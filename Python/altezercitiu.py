52253057f90a3c0aed19b8e1a5d939574e42ae98
a mihaita <amihaita@yahoo.com>
2017-03-10 18:24:10 +0200

    am comis-o ca un comisar

--- Python/altezercitiu.py
+++ Python/altezercitiu.py
@@ -1,16 +1,19 @@
-def lines_startswith(file, letter):
-    """ (file open for reading, str) -> list of str
+def count_chars(s):
+    '''(str) -> dict of {str: int}
 
-    Return the list of lines from file that begin with letter.     The lines should have thenewline removed.
+    Return a dictionary where the keys are the characters in s and the values
+    are how many times those characters appear in s.
 
-   Precondition: len(letter) == 1
-   """
+    >>> count_chars('abracadabra')
+    {'a': 5, 'r': 2, 'b': 2, 'c': 1, 'd': 1}
+    '''
+    d = {}
 
+    for c in s:
+        if not (c in d):
+            d[c] = 1 # CODE MISSING HERE
+        else:
+            d[c] = d[c] + 1
 
-    matches = []
-
-    # CODE MISSING HERE
-    for line in file:
-        if letter == line[0]:
-            matches.append(line.rstrip('\n'))
-    return matches
+    return d
+print(count_chars('abracadabra'))
