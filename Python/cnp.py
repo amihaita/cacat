d56b67397418abe620a9ff71cc4eddfe6baeb77a
amihaita <amihaita@yahoo.com>
2017-03-12 16:20:18 +0200

    blablablabla

--- Python/cnp.py
+++ Python/cnp.py
@@ -1,53 +1,49 @@
 cnp = input("Introduceți CNP: ")
-try:
-    listcnp = list(map(int,cnp))
-except ValueError:
-    exit("Nu ati introdus cifre! CNP conține doar cifre!")
 comparednum = "279146358279"
-listcn = list(map(int,comparednum))
-comparedcnp = cnp[0:13]
-zicnp = int(cnp[5:7])
-lunacnp = int(cnp[3:5])
-ancnp = int(cnp[1:3])
-sexnum = int(cnp[0])
-reprsec = cnp[0]
+try:
+    listcnp = list(map(int, cnp))
+    listcn = list(map(int, comparednum))
+    comparedcnp = cnp[0:13]
+    zicnp = int(cnp[5:7])
+    lunacnp = int(cnp[3:5])
+    ancnp = int(cnp[1:3])
+    sexnum = int(cnp[0])
+    reprsec = cnp[0]
+except ValueError:
+    exit("Format CNP incorect! CNP trebuie să conțină 13 cifre!")
 maxziluna = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+regiunea = {"01": "Alba", "02": "Arad", "03": "Argeș", "04": "Bacău", "05": "Bihor", "06": "Bistrița Năsăud",
+            "07": "Botoșani", "08": "Brașov", "09": "Brăila", "10": "Buzău", "11": "Caraș Severin", "12": "Cluj",
+            "13": "Constanța", "14": "Covasna", "15": "Dambovița", "16": "Dolj", "17": "Galați", "18": "Gorj",
+            "19": "Harghita", "20": "Hunedoara", "21": "Ialomița", "22": "Iași", "23": "Ilfov", "24": "Maramureș",
+            "25": "Mehedinți", "26": "Mureș", "27": "Neamț", "28": "Olt", "29": "Prahova", "30": "Satu Mare",
+            "31": "Sălaj", "32": "Sibiu", "33": "Suceava", "34": "Teleorman", "35": "Timiș", "36": "Tulcea",
+            "37": "Vaslui", "38": "Vâlcea", "39": "Vrancea", "40": "București", "41": "București Sectorul 1",
+            "42": "București Sectorul 2", "43": "București Sectorul 3", "44": "București Sectorul 4",
+            "45": "București Sectorul 5", "46": "București Sectorul 6", "51": "Călărași", "52": "Giurgiu"}
 
 
-def checklcnp(param):
-    if len(param) != 13:
+def checklcnp(cnp):
+    if len(cnp) != 13:
         exit("Lungime CNP incorectă, CNP trebuie să aibă 13 caractere!")
-checklcnp(cnp)
+    return len(cnp)
 
 
-def checkluna(param):
-    if lunacnp not in range(1, 13):
-        exit("Caracterele 4 și 5 incorecte! Luna nu este corectă, anul are 12 luni!")
-checkluna(cnp)
-
-
-def checkzi(param):
-    if zicnp not in range(1, 32):
-        exit("Caracterele 6 și 7 incorecte! Lunile anului pot avea maxim 31 de zile.")
-    if zicnp not in range(1, maxziluna[lunacnp]+1):
-        print("Ziua nu este corectă!")
-checkzi(cnp)
-
-
-def checksex(param):
+def checksex(cnp):
     if sexnum == 0:
-        print("CNP nu poate să începă cu 0")
+        sex = "Cifra corespunzătoare pentru sex incorectă"
     elif sexnum in [1, 3, 5, 7]:
-        print("sex masculin")
+        sex = "sex masculin"
     elif sexnum in [2, 4, 6, 8]:
-        print("sex feminin")
+        sex = "sex feminin"
     else:
-        print("cetățean străin nerezident - nu se poate preciza sexul")
-checksex(cnp)
+        sex = "cetățean străin nerezident - nu se poate preciza sexul"
+    return sex
 
 
-def checkan(param):
+def checkan(cnp):
     global secol
+    global an
     try:
         if int(reprsec) in range(1, 3):
             secol = 19
@@ -55,14 +51,37 @@ def checkan(param):
             secol = 18
         elif int(reprsec) in range(5, 7):
             secol = 20
-        print("Persoana născută în %02d.%02d.%s%s" % (zicnp, lunacnp, secol, ancnp))
-        return secol
+        an = int(str(secol) + cnp[1:3])
+        return secol+1
     except NameError:
-        print("Secvența ce conține data nașterii este incorectă!")
-checkan(cnp)
+        exit("Cifra corespunzătoare pentru secolul nașterii este incorectă!")
 
 
-def control(param):
+
+def checkluna(cnp):
+    if lunacnp not in range(1, 13):
+        exit("Luna nu este corectă, anul are 12 luni! Ați introdus %s pentru lună!" % lunacnp)
+    return lunacnp
+
+def checkzi(cnp):
+    try:
+        if zicnp not in range(1, maxziluna[lunacnp]+1):
+            print("Ziua nu este corectă! Luna %s are %s zile" %(lunacnp, maxziluna[lunacnp]))
+    except KeyError:
+        print("Zi naștere incorectă")
+    return zicnp
+
+
+def checkregiune(cnp):
+    print("Verificare regiune: ...")
+    try:
+        return regiunea[cnp[7:9]]
+    except KeyError:
+        print("Cod regiune greșit")
+
+
+def control(cnp):
+    print("Verificare sumă control: ...")
     total = 0
     rez = [x * y for x, y in zip(listcn, listcnp)]
     for i in range(len(rez)):
@@ -70,9 +89,23 @@ def control(param):
     sc = total % 11
     if sc == 10:
         sc -= 9
-    print(sc)
-    print(cnp[12])
     if sc != int(cnp[12]):
-        print("CNP INCORECT!!! Suma de control nu corespunde!")
-control(cnp)
+        mesaj = "CNP INCORECT!!! Suma de control nu corespunde!"
+        exit("CNP INCORECT!!! Suma de control nu corespunde!")
+    else:
+        mesaj = "CNP CORECT!!! Suma de control corespunde!"
+    return mesaj
 
+print("Verificare lungime CNP: ...")
+print(checklcnp(cnp))
+print("Verificare secvență sex: ...")
+print(checksex(cnp))
+print("Verificare secol dată naștere: ...")
+print(checkan(cnp))
+print("Verificare lună naștere: ...")
+print(checkluna(cnp))
+print("Verificare zi naștere: ...")
+print(checkzi(cnp))
+print(checkregiune(cnp))
+print(control(cnp))
+print("Persoana născută în %02d.%02d.%d" % (zicnp, lunacnp, an))
